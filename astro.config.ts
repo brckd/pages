63a44440ba91a2c17d3e49b0b96ce02d5ad8eba1
brckd <brycked@proton.me>
2023-08-29 20:05:05 +0200

    feat: fill in blog template

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import "dotenv/config";
 
 const url = process.env.URL && new URL(process.env.URL);
@@ -6,4 +8,5 @@ const url = process.env.URL && new URL(process.env.URL);
 export default defineConfig({
   site: url && url.origin,
   base: url && url.pathname,
+  integrations: [mdx(), sitemap()],
 });
