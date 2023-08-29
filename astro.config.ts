f34bd72424eec7498270c4d3c5e9c693a5dc68f9
brckd <brycked@proton.me>
2023-08-29 20:20:55 +0200

    fix: hardcode canonical url

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import "dotenv/config";
-
-const url = process.env.URL && new URL(process.env.URL);
 
 export default defineConfig({
-  site: url && url.origin,
-  base: url && url.pathname,
+  site: "https://bricked.vercel.app",
   integrations: [mdx(), sitemap()],
 });
