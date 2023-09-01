d889620ae69b6cb946bfc34461a2b8cfd4301673
brckd <brycked@proton.me>
2023-09-01 22:53:28 +0200

    chore: update domain and email

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://bricked.vercel.app",
+  site: "https://bricked.dev",
   integrations: [mdx(), sitemap()],
   experimental: {
     viewTransitions: true,
