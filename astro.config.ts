6f3a684970240b1b9c920caea549cdfbed18c49c
bricked <git@bricked.dev>
2023-09-02 04:12:55 +0200

    chore: upgrade to astro 3

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URI } from "./src/consts";
 
 export default defineConfig({
   site: SITE_URI,
-  integrations: [mdx(), sitemap()],
-  experimental: {
-    viewTransitions: true,
-  },
+  integrations: [sitemap()],
 });
