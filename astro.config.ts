a42147a4929aa3eff444a3cb64071852d97ec095
bricked <git@bricked.dev>
2023-09-03 20:31:11 +0200

    build: add astro compressor

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URI } from "./src/consts";
+import compressor from "astro-compressor";
 
+// https://astro.build/config
 export default defineConfig({
   site: SITE_URI,
-  integrations: [sitemap()],
+  integrations: [sitemap(), compressor()],
 });
