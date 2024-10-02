6675716c34e7d9a3b32b9b0c5a09f6b13751afb4
bricked <git@bricked.dev>
2024-10-02 12:09:49 +0200

    refactor: upgrade to astro 5.0
    Short URL redirects such as /gh are now deleted
    VSCode config are also removed.
    Astro and dependencies are being upgraded.

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import {
   SITE_URI,
-  DISCORD_URI,
-  GITHUB_URI,
-  MATRIX_URI,
-  EMAIL_URI,
-  MUES_URI,
-  ARTCADE_URI,
 } from "./src/consts";
 import compressor from "astro-compressor";
 
@@ -15,12 +9,4 @@ import compressor from "astro-compressor";
 export default defineConfig({
   site: SITE_URI,
   integrations: [sitemap(), compressor()],
-  redirects: {
-    "/dc": DISCORD_URI,
-    "/gh": GITHUB_URI,
-    "/mx": MATRIX_URI,
-    "/em": EMAIL_URI,
-    "/mu": MUES_URI,
-    "/ac": ARTCADE_URI,
-  },
 });
