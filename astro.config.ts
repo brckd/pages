4206d6e7ce7664120af2492f1502adfadeab7802
bricked <git@bricked.dev>
2025-02-13 15:36:42 +0100

    feat: add favicon and pwa support

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "astro/config";
 import { SITE_URI } from "./src/consts";
+import favicons from "astro-favicons";
 
 export default defineConfig({
   site: SITE_URI,
+  integrations: [
+    favicons({
+      input: "src/assets/icons/icon.png",
+      name: "Bricked",
+      short_name: "Bricked",
+    }),
+  ],
 });
