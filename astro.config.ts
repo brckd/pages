75fbcc0be392a41d58faac78ff29be011ff26371
bricked <git@bricked.dev>
2025-02-16 23:31:58 +0100

    revert: 7254b96
    This reverts commit 7254b967f42a5929788443f61c69eb2a9afb762d.

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
-import { SITE_URI } from "./src/consts";
+import { SITE_URI, SITE_TITLE } from "./src/consts";
+import favicons from "astro-favicons";
 import mdxConfig from "./src/lib/markdownConfig";
 
 import vercel from "@astrojs/vercel";
@@ -10,6 +11,13 @@ const adapter = env.ADAPTER?.toLowerCase();
 
 export default defineConfig({
   site: SITE_URI,
+  integrations: [
+    favicons({
+      input: "src/assets/icons/icon.png",
+      name: SITE_TITLE,
+      short_name: SITE_TITLE,
+    }),
+  ],
   markdown: mdxConfig,
   adapter:
     adapter === "netlify"
