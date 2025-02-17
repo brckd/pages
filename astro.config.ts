27d8ff89cadf12bff5f17f0c22905149dce9dc38
bricked <git@bricked.dev>
2025-02-17 20:24:49 +0100

    revert: bc0e671
    This reverts commit bc0e671b6765b97caa4b00bfab588c66dfe3b1fc.

--- astro.config.ts
+++ astro.config.ts
@@ -1,35 +1,23 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import shield from "@kindspells/astro-shield";
+import markdown from "./src/lib/markdownConfig";
+import { SITE_URI } from "./src/consts";
 
 import { loadEnv } from "vite";
 import vercel from "@astrojs/vercel";
 import netlify from "@astrojs/netlify";
-const adapters = {
-  netlify: netlify(),
-  vercel: vercel(),
-};
-
-import markdown from "./src/lib/markdownConfig";
-import { SITE_URI } from "./src/consts";
 
 const env = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
-const _adapter = env.ADAPTER?.toLowerCase();
-const adapter =
-  _adapter === "netlify" || _adapter === "vercel" ? _adapter : undefined;
+const adapter = env.ADAPTER?.toLowerCase();
 
 export default defineConfig({
   site: SITE_URI,
   markdown,
-  integrations: [
-    mdx(),
-    shield({
-      securityHeaders: {
-        enableOnStaticPages: adapter && {
-          provider: adapter,
-        },
-      },
-    }),
-  ],
-  adapter: adapter && adapters[adapter],
+  integrations: [mdx()],
+  adapter:
+    adapter === "netlify"
+      ? netlify()
+      : adapter === "vercel"
+        ? vercel()
+        : undefined,
 });
