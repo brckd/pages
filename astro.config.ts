b134a5e169e6835d2c3e7f87712046c3b374d828
bricked <git@bricked.dev>
2025-02-17 02:20:30 +0100

    feat(blog): add images

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,19 @@
-import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import markdown from "./src/lib/markdownConfig";
 import { SITE_URI } from "./src/consts";
-import mdxConfig from "./src/lib/markdownConfig";
 
+import { loadEnv } from "vite";
 import vercel from "@astrojs/vercel";
 import netlify from "@astrojs/netlify";
+
 const env = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
 const adapter = env.ADAPTER?.toLowerCase();
 
 export default defineConfig({
   site: SITE_URI,
-  markdown: mdxConfig,
+  markdown,
+  integrations: [mdx()],
   adapter:
     adapter === "netlify"
       ? netlify()
