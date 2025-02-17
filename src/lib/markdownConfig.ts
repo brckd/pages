8dc3cfd28ab7501944f3aed34559aab67469d94e
bricked <git@bricked.dev>
2025-02-17 22:19:24 +0100

    feat: add rehype autolink headings

--- src/lib/markdownConfig.ts
+++ src/lib/markdownConfig.ts
@@ -1,28 +1,55 @@
-import rehypeSanitize from "rehype-sanitize";
-import rehypeStringify from "rehype-stringify";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkReadingTime from "./remarkReadingTime";
 import remarkParse from "remark-parse";
+import remarkReadingTime from "./remarkReadingTime";
+import remarkFrontmatter from "remark-frontmatter";
 import remarkRehype from "remark-rehype";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings, {
+  type Options as RehypeAutolinkHeadingsOptions,
+} from "rehype-autolink-headings";
+import rehypeStringify from "rehype-stringify";
 import { unified } from "unified";
 
-export const remarkPlugins = [remarkReadingTime, remarkFrontmatter];
-export const rehypePlugins = [rehypeSanitize];
+export const rehypeAutolinkHeadingsOptions: RehypeAutolinkHeadingsOptions = {
+  behavior: "prepend",
+  content: {
+    type: "text",
+    value: "#",
+  },
+  properties: {
+    class: "heading-anchor",
+  },
+};
+
+export const remarkPlugins = [remarkReadingTime, remarkFrontmatter] as const;
+export const rehypePlugins = [
+  rehypeSlug,
+  rehypeSanitize,
+  [rehypeAutolinkHeadings, rehypeAutolinkHeadingsOptions],
+] as const;
 
 export const processor = unified();
 processor.use(remarkParse);
 for (const plugin of remarkPlugins) {
-  processor.use(plugin);
+  if (Array.isArray(plugin)) {
+    processor.use(...plugin);
+  } else {
+    processor.use(plugin);
+  }
 }
 processor.use(remarkRehype);
 for (const plugin of rehypePlugins) {
-  processor.use(plugin);
+  if (Array.isArray(plugin)) {
+    processor.use(...plugin);
+  } else {
+    processor.use(plugin);
+  }
 }
 processor.use(rehypeStringify);
 
 export const compileOptions = {
-  rehypePlugins,
   remarkPlugins,
-};
+  rehypePlugins,
+} as const;
 
 export default compileOptions;
