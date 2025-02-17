bc0e671b6765b97caa4b00bfab588c66dfe3b1fc
bricked <git@bricked.dev>
2025-02-17 19:49:55 +0100

    feat: add csp with astro-shield

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,35 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import markdown from "./src/lib/markdownConfig";
-import { SITE_URI } from "./src/consts";
+import shield from "@kindspells/astro-shield";
 
 import { loadEnv } from "vite";
 import vercel from "@astrojs/vercel";
 import netlify from "@astrojs/netlify";
+const adapters = {
+  netlify: netlify(),
+  vercel: vercel(),
+};
+
+import markdown from "./src/lib/markdownConfig";
+import { SITE_URI } from "./src/consts";
 
 const env = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
-const adapter = env.ADAPTER?.toLowerCase();
+const _adapter = env.ADAPTER?.toLowerCase();
+const adapter =
+  _adapter === "netlify" || _adapter === "vercel" ? _adapter : undefined;
 
 export default defineConfig({
   site: SITE_URI,
   markdown,
-  integrations: [mdx()],
-  adapter:
-    adapter === "netlify"
-      ? netlify()
-      : adapter === "vercel"
-        ? vercel()
-        : undefined,
+  integrations: [
+    mdx(),
+    shield({
+      securityHeaders: {
+        enableOnStaticPages: adapter && {
+          provider: adapter,
+        },
+      },
+    }),
+  ],
+  adapter: adapter && adapters[adapter],
 });
