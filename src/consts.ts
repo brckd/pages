dc6a829ead250211f8bdfcd24da641bae5615f81
bricked <git@bricked.dev>
2025-02-19 18:58:06 +0100

    feat: under the hood accessibility improvements

--- src/consts.ts
+++ src/consts.ts
@@ -5,5 +5,7 @@ export const DOMAIN = "bricked.dev";
 export const SITE_URI = `https://${DOMAIN}`;
 export const SITE_TITLE = "Bricked";
 export const SITE_DESCRIPTION = "Welcome to bricked's personal website!";
+export const SOURCE_REPOSITORY = "https://github.com/brckd/pages";
+export const LICENSE_URI = `${SOURCE_REPOSITORY}/blob/main/LICENSE.md`;
 export const THEME_COLOR = "#89b4fa"; // Catppuccin Mocha blue
 export const BACKGROUND_COLOR = "#1e1e2e"; // Catppuccin Mocha mantle
